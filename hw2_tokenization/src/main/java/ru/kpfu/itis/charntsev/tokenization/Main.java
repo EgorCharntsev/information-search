3a9c92f663319263566a6fa3691d0cf58d05276e
EgorCharntsev <charncev200447@gmail.com>
2026-03-12 19:38:05 +0300

    init directory for task 2

---
+++ hw2_tokenization/src/main/java/ru/kpfu/itis/charntsev/tokenization/Main.java
@@ -0,0 +1,12 @@
+package ru.kpfu.itis.charntsev.crawler;
+
+import ru.kpfu.itis.charntsev.crawler.config.ConfigLoader;
+import ru.kpfu.itis.charntsev.crawler.config.CrawlConfig;
+import ru.kpfu.itis.charntsev.crawler.crawl.CrawlEngine;
+import ru.kpfu.itis.charntsev.crawler.crawl.CrawlResult;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+    }
+}
