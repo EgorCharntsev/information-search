0dfaa82508e899df9d1b7f56c40a9556cb5bcadf
EgorCharntsev <charncev200447@gmail.com>
2026-03-28 14:27:24 +0300

    init branch for task 5

---
+++ hw5_search_engine/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    application
+}
+
+dependencies {}
+
+application {
+    mainClass.set("ru.kpfu.itis.charntsev.search.engine.Main")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    standardInput = System.`in`
+    jvmArgs(
+        "-Dfile.encoding=UTF-8",
+        "-Dsun.stdout.encoding=UTF-8",
+        "-Dsun.stderr.encoding=UTF-8"
+    )
+}
+
+tasks.register("prepareKotlinBuildScriptModel") {
+    doLast { }
+}
